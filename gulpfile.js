cd0ee203cf6f99722c11035e47acb5d9c566f941
Diego Barrado <Diego.barrado@gmail.com>
2025-02-14 21:44:59 -0300

    atualização 0.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const browserSync = require('browser-sync').create();
-const autoprefixer = require('gulp-autoprefixer');
-const cssnano = require('gulp-cssnano');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const del = require('del');
-const cleanCSS = require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 
-function sass(){
-    return gulp.src('src/scss/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle: 'compressed',
+    function construirsass(){
+        return gulp.src('src/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed',
     }))
-    .pipe(sourcemaps.write('/maps'))
-    .pipe(gulp.dest('src/css'))
-}
-function minifyImg (){
-    return gulp.src('src/img/*')
-    .pipe(imagemini())
-    .pipe(gulp.dest('src/img'))
-}
+        .pipe(sourcemaps.write('/maps'))
+        .pipe(gulp.dest('src/css'));
+    }
+    exports.CompilAsass = construirsass;
 exports.watch = function(){
-    gulp.watch('src/sass/*.scss', {ignoreInitial:false}, gulp.series(sass))
+    gulp.watch('src/scss/*.scss', {ignoreInitial:false}, gulp.series(construirsass))
 }
