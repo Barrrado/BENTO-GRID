27d1c1012097de61d909796d524863e175c85f98
Diego Barrado <Diego.barrado@gmail.com>
2025-02-16 13:19:39 -0300

    Desafio atualizado!

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
+const minifyImg = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
+const cssmini = require('gulp-cssnano');
 
-    function construirsass(){
+
+    function styles(){
         return gulp.src('src/scss/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass({
@@ -11,7 +14,25 @@ const sourcemaps = require('gulp-sourcemaps');
         .pipe(sourcemaps.write('/maps'))
         .pipe(gulp.dest('src/css'));
     }
-    exports.CompilAsass = construirsass;
-exports.watch = function(){
-    gulp.watch('src/scss/*.scss', {ignoreInitial:false}, gulp.series(construirsass))
-}
+    function minify (){
+        return gulp.src('./assets/imagens/*')
+        .pipe(minifyImg())
+        .pipe(gulp.dest('src/img'))
+    }
+
+    function minifycss (){
+        return gulp.src('src/css/*.css')
+        .pipe(cssmini())
+        .pipe(gulp.dest('src/css'))
+        }
+    function minifyjs (){
+        return gulp.src('assets/js/*.js')
+        .pipe(jsmin())
+        .pipe(gulp.dest('src/js'))
+        }
+
+        exports.watch = function(){
+            gulp.watch('src/scss/*.scss', {ignoreInitial:false},gulp.series(styles));
+            gulp.watch('assets/imagens/*',{ignoreInitial:false}, gulp.series(minify));
+            gulp.watch('src/css/*.css',{ignoreInitial:false}, gulp.series(minifycss));
+        }         
